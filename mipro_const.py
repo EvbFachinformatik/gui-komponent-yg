7e7006651c138efc631ea9075e43c194bec6a06d
EvbFachinformatik <144215683+EvbFachinformatik@users.noreply.github.com>
2023-09-06 15:52:53 +0200

    Add files via upload

---
+++ mipro_const.py
@@ -0,0 +1,16 @@
+# Wiederkehrende und häufig verwendete Werte für die Umsetzung des
+# MIPRO-Projekts als Konstanten in separater Source definiert,
+# um sie anschließend in jedem benötigten Modul einzubinden.
+
+# Abschnitt: GUI-Definitionen
+FENSTERGROESSE = "600x600"
+HINTERGRUNDFARBE = "#E4F1FF"
+BUTTONFARBE_BG = "#176B87"
+BUTTONFARBE_FG = "white"
+
+PROGRAMMNAME = "MIPRO"
+PROGRAMMVERSION = "1.0"
+
+# Abschnitt: SQL-Datenbank Zugriff
+
+# Abschnitt: allgemeine Konstanten für Python
